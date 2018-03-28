423004cbb88ce963b182e3c3e14967c6018194dd
Petar Markov <petar.markov@htecgroup.com>
2018-03-28 14:08:16 +0200

    init

---
+++ lib/index.ts
@@ -0,0 +1,2 @@
+export { NestableComponent } from './src/nestable.component';
+export { NestableModule } from './src/nestable.module';
---
+++ lib/src/nestable.models.ts
@@ -0,0 +1,25 @@
+export interface NestableSettings {
+    listNodeName: string;
+    itemNodeName: string;
+    handleNodeName: string;
+    contentNodeName: string;
+    rootClass: string;
+    listClass: string;
+    itemClass: string;
+    dragClass: string;
+    handleClass: string;
+    contentClass: string;
+    collapsedClass: string;
+    placeClass: string;
+    noDragClass: string;
+    noChildrenClass: string;
+    emptyClass: string;
+    expandBtnHTML: string;
+    collapseBtnHTML: string;
+    group: number;
+    maxDepth: number;
+    threshold: number;
+    fixedDepth: boolean;
+    fixed: boolean;
+    exportCollapsed: boolean;
+}
