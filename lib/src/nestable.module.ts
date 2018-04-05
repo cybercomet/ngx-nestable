7446037402bd8bbd1494d9d5818594999e1f1fc5
Mihajlo Grubjesic <mihajlo.grubjesic@htecgroup.com>
2018-04-05 15:44:42 +0200

    remove: remove expand directive

--- lib/src/nestable.module.ts
+++ lib/src/nestable.module.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { NestableComponent } from './nestable.component';
 import { NestableDragHandleDirective } from './nestable-drag-handle/nestable-drag-handle.directive';
-import { NestableExpandDirective } from './nestable-expand/nestable-expand.directive';
 
 @NgModule({
     imports: [CommonModule],
-    declarations: [NestableComponent, NestableDragHandleDirective, NestableExpandDirective],
-    exports: [NestableComponent, NestableDragHandleDirective, NestableExpandDirective]
+    declarations: [
+        NestableComponent,
+        NestableDragHandleDirective
+    ],
+    exports: [
+        NestableComponent,
+        NestableDragHandleDirective
+    ]
 })
 export class NestableModule { }
